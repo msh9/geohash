a5609b52669dc84b4b988ca452e4652ce8d4bf9b
Michael Hughes <michael@mihughes.com>
2017-03-26 21:46:50 -0600

    Add a working to/from lat,lng geohash implementation

--- src/geocomponent/GeoHashLib.js
+++ src/geocomponent/GeoHashLib.js
@@ -4,7 +4,7 @@
  * @module
  */
 
-const geohashBase32 = [
+const geohashBase32Indices = [
     '0',
     '1',
     '2',
@@ -38,6 +38,48 @@ const geohashBase32 = [
     'y',
     'z'
 ];
+
+//I will admit, this looks really stupid, but it does save us from
+//executing #indexOf or similar many times against geohashBase32Indices
+//when converting from a geohash.
+const geohashBase32CharMap = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'b': 10,
+    'c': 11,
+    'd': 12,
+    'e': 13,
+    'f': 14,
+    'g': 15,
+    'h': 16,
+    'j': 17,
+    'k': 18,
+    'm': 19,
+    'n': 20,
+    'p': 21,
+    'q': 22,
+    'r': 23,
+    's': 24,
+    't': 25,
+    'u': 26,
+    'v': 27,
+    'w': 28,
+    'x': 29,
+    'y': 30,
+    'z': 31
+};
+
+const maxLatitudeBound = 90;
+const maxLongitudeBound = 180;
+const encodeBits = 5;
 
 /**
  * Point defines a WGS84 Coordinate System Point
@@ -74,8 +116,17 @@ export class Point {
      * Calculates the 12 character geohash for this point
      * @returns {string} The geohash for this point
      */
-    geohash() {
+    toGeohash() {
         return geohashFromLatLng(this._latitude, this._longitude);
+    }
+
+    /**
+     * Returns a point that represents the average WGS84 location of a geohash box
+     * @param {string} geohash The geohash used to calculated a latitude longitude pair
+     * @returns {Point} The mid-point of the latitude longitude box
+     */
+    static fromGeohash(geohash) {
+        return latLngFromGeohash(geohash);
     }
 }
 
@@ -83,12 +134,94 @@ export class Point {
  * Returns a geohash string built from a WGS84 latitude longitude pair
  * @param {number} latitude A WGS84 latitude
  * @param {number} longitude A WGS84 longitude
- * @param {number=12} precision The level of precision to encode
+ * @param {number} [precision=12] The level of precision to encode, defaults to 12 characters
  * @returns {string} A geohash string
  */
 export function geohashFromLatLng(latitude, longitude, precision=12) {
     if (precision < 1) {
         throw new Error('Precision value must be 1 or greater');
     }
+    let doEven = true, lowerLatitudeBound = maxLatitudeBound * -1, lowerLongitudeBound = maxLongitudeBound * -1,
+        upperLatitudeBound = maxLatitudeBound, upperLongitudeBound = maxLongitudeBound, fiveBitResult = [],
+        encodedResult = [];
+    //NB: This loop is 1-indexed so we don't to add one each time we check to see if a base32 character needs to be
+    //encoded.
+    for (let i = 1; i <= precision * encodeBits; i++) {
+        let midLongitude = (lowerLongitudeBound + upperLongitudeBound) / 2;
+        let midLatitude = (lowerLatitudeBound + upperLatitudeBound) / 2;
 
+        if (doEven) { //evens are for longitude
+            let position = (longitude < midLongitude) ? 0 : 1;
+            fiveBitResult.push(position.toString());
+            if (position) {
+                lowerLongitudeBound = midLongitude;
+            } else {
+                upperLongitudeBound = midLongitude;
+            }
+        } else {
+            let position = (latitude < midLatitude) ? 0 : 1;
+            fiveBitResult.push(position.toString());
+             if (position) {
+                lowerLatitudeBound = midLatitude;
+            } else {
+                upperLatitudeBound = midLatitude;
+            }
+        }
+
+        if (i % encodeBits === 0) {
+            //Please let me know if there is better JS trickery to get from an array of 1/0s in base 2 to a base 10 number
+            const charVal = Number.parseInt(fiveBitResult.join(''), 2);
+            if (charVal < 0 || charVal > 31) {
+                throw new Error('Parsing latitude and longitude resulted in invalid base32 value');
+            }
+            encodedResult.push(geohashBase32Indices[charVal]);
+            fiveBitResult.length = 0;
+        }
+
+        doEven = !doEven
+    }
+
+    return encodedResult.join('');
+}
+
+/**
+ * Returns an approximate WGS84 point based on a given geohash
+ * @param {string} geohash A geohash to decode into an approximate latitude and longitude
+ * @returns {Point} The approximate point represented by the geohash
+ */
+export function latLngFromGeohash(geohash) {
+   if (geohash === null || geohash.length < 1) {
+       throw new Error('Empty geohash is not valid.');
+   }
+   let doEven = true, lowerLatitudeBound = maxLatitudeBound * -1, lowerLongitudeBound = maxLongitudeBound * -1,
+        upperLatitudeBound = maxLatitudeBound, upperLongitudeBound = maxLongitudeBound;
+   let midLongitude = (lowerLongitudeBound + upperLongitudeBound) / 2,
+        midLatitude = (lowerLatitudeBound + upperLatitudeBound) / 2;
+   for (let char of geohash) {
+       const baseTenEncoded = geohashBase32CharMap[char];
+       if (baseTenEncoded === undefined) {
+           throw new Error(`Invalid character in geohash, cannot convert ${char}`);
+       }
+       //why? because strings and binary don't mix well and we need left padding zeros in order for our calculations
+       //to work
+       const baseTwoEncoded = ('0000' + baseTenEncoded.toString(2)).slice(-5);
+       for (let binaryChar of baseTwoEncoded) {
+           console.log(`here - ${binaryChar} from ${baseTwoEncoded}`);
+           if (doEven && binaryChar === '1') {
+               lowerLongitudeBound = midLongitude;
+           } else if (doEven) {
+               upperLongitudeBound = midLongitude;
+           } else if (binaryChar === '0') {
+               upperLatitudeBound = midLatitude;
+           } else {
+               lowerLatitudeBound = midLatitude;
+           }
+
+           midLongitude = (lowerLongitudeBound + upperLongitudeBound) / 2;
+           midLatitude = (lowerLatitudeBound + upperLatitudeBound) / 2;
+           doEven = !doEven
+       }
+   }
+
+   return new Point(midLatitude, midLongitude);
 }
--- src/geocomponent/GeoHashLib.test.js
+++ src/geocomponent/GeoHashLib.test.js
@@ -1,4 +1,4 @@
-import { Point, geohashFromLatLng } from './GeoHashLib.js';
+import { Point, geohashFromLatLng, latLngFromGeohash } from './GeoHashLib.js';
 
 describe('Convert to geohash from latitude and longitude', () => {
    test('Rejects precision less than 1', () => {
@@ -15,10 +15,48 @@ describe('Convert to geohash from latitude and longitude', () => {
        expect(geohashFromLatLng(-25.382708, -49.265506, 12)).toBe('6gkzwgjzn820');
    });
 
+   test('Equals to mid-point yields 1 bit', () => {
+       expect(geohashFromLatLng(0, 0, 4)).toBe('s000');
+   });
+
+   test('A value just west and south of 0,0 yields leading zeros then ones', () => {
+       expect(geohashFromLatLng(-0.01, -0.01, 1)).toBe('7');
+   });
+
    describe('Point class based conversion', () => {
         test('Encode using using a point', () => {
              let point = new Point(-25.382708, -49.265506);
-             expect(point.geohash()).toBe('6gkzwgjzn820');
+             expect(point.toGeohash()).toBe('6gkzwgjzn820');
         });
    });
+});
+
+describe('Convert to latitude and longitude from geohash', () => {
+    test('Rejects empty geohash string', () => {
+       expect(() => {
+           latLngFromGeohash('');
+       }).toThrow();
+    });
+
+    test('Rejects a geohash with an invalid character', () => {
+        expect(() => {
+            latLngFromGeohash('-');
+        }).toThrow();
+    });
+
+    test('accurately decodes a single character geohash', () => {
+        expect(latLngFromGeohash('y')).toMatchObject(new Point(67.5, 112.5));
+    });
+
+    test('accurately decides a five character geohash', () => {
+        expect(latLngFromGeohash('ezs42')).toMatchObject(new Point(42.60498046875,-5.60302734375 ));
+    });
+
+    test('accurately decodes a 12 character geohash', () => {
+        expect(latLngFromGeohash('6gkzwgjzn820')).toMatchObject(new Point(-25.38270807825029, -49.265506099909544));
+    });
+});
+
+test('conversion round-tripping works as expected', () => {
+    expect(latLngFromGeohash(geohashFromLatLng(-25.38270807825029,-49.265506099909544, 12))).toMatchObject(new Point(-25.38270807825029,-49.265506099909544));
 });
