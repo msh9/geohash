af24c5088cd221b4b551c5bbe047f33ab18178b1
Michael Hughes <michael@mihughes.com>
2017-04-01 20:03:19 -0600

    start work on the visual component

---
+++ src/geocomponent/GeoHashConverter.js
@@ -0,0 +1,16 @@
+import Component from 'react';
+import { Point } from './GeoHashLib.js';
+import
+
+export class GeoHashConverter extends Component {
+    render() {
+        return (
+            <div id="geo-hash-converter">
+                <h3>Converts from a geohash to a latitude and longitude or converts from latitude and longitude to a geohash</h3>
+                <div>
+
+                </div>
+            </div>
+        )
+    }
+}
