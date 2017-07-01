c5a22dbb7ec95539fa3667c02ccc2d3195343d55
Michael Hughes <michael@mihughes.com>
2017-06-30 20:13:34 -0600

    rendered form

--- src/views/converter.js
+++ src/views/converter.js
@@ -2,11 +2,37 @@
 import m from 'mithril';
 
 export const converterView = {
-  view: function() {
+  view: function () {
     return m('.geo-converter', [
       m('form', [
         m('select', {autofocus: true}, [
           m('option', {value: 'geohash'}, 'GeoHash'),
+          m('option', {value: 'georef'}, 'GeoRef'),
+          m('option', {value: 'mhlocator'}, 'MH Locator'),
+        ]),
+        m('label', [
+          'Latitude:',
+          m('input',
+            {
+              type: 'number',
+              max: 180,
+              min: -180,
+              require: true,
+              name: 'latitude',
+              placeholder: 0.0
+            }),
+        ]),
+        m('label', [
+          'Longitude:',
+          m('input',
+            {
+              type: 'number',
+              max: 180,
+              min: -180,
+              require: true,
+              name: 'longitude',
+              placeholder: 0.0
+            }),
         ]),
       ]),
     ]);
